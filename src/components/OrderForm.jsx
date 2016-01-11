405a31e67989ab05f714a6e4a4f97331970254af
flaviusone <flaviusone@gmail.com>
2016-01-12 00:47:37 +0200

    Add new OrderForm component

---
+++ src/components/OrderForm.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+require('../styles/order-form.less');
+
+/**
+ * Order form for room
+ * @param  {Object[]} menu  The food menu
+ */
+module.exports = React.createClass({
+
+  render: function() {
+    return <div className='order-form'>
+      Brebex
+    </div>;
+  }
+});
