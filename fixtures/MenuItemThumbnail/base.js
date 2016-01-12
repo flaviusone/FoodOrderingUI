127c3e631b86b691575b05905e7e7b7974603364
flaviusone <flaviusone@gmail.com>
2016-01-12 16:36:05 +0200

    Add MenuItemThumbnail

---
+++ fixtures/MenuItemThumbnail/base.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Platou cald pentru 2 persoane ',
+  image: require('../../assets/img/menu_placeholder.png'),
+  description: 'calamar pane,creveti in cuib de cartof, midii pane,pui de balta pane, rapane ,somon pane, mouslitos, creveti din surimi,cartofi prajiti',
+  quantity: '800g ',
+  price: ' 60,00 RON '
+};
---
+++ src/components/MenuItemThumbnail.jsx
@@ -0,0 +1,62 @@
+var React = require('react'),
+    classSet = require('classnames');
+
+require('../styles/menuitem-thumbnail.less');
+
+/**
+ * Renders room thumbnail
+ * @param {String} name
+ * @param {String} image
+ * @param {String} description
+ * @param {String} quantity
+ * @param {String} price
+ * @param {Boolean} oddBackground Dictates thumbnail color
+ * @param {Function} onItemAdd callback
+ * @param {Function} onItemSubstract callback
+ */
+module.exports = React.createClass({
+
+  render: function() {
+    var classes = classSet({
+      'menuitem-thumbnail': true,
+      'oddBackground': this.props.oddBackground
+    });
+    return <div className={classes}>
+      {this.props.image ? this._renderLogo() : null}
+      {this._renderInfo()}
+    </div>;
+  },
+
+  _renderLogo: function() {
+    return <img className="menuitem-thumbnail-logo" src={this.props.image}/>;
+  },
+
+  _renderInfo: function() {
+    return <div className="menuitem-thumbnail-info">
+      <p className="menuitem-thumbnail-title">
+        {this.props.name}
+      </p>
+      <p className="menuitem-thumbnail-description">
+        {this.props.description}
+      </p>
+      <div className="menuitem-thumbnail-numbers">
+        <p className="menuitem-thumbnail-quantity">
+          {this.props.quantity}
+        </p>
+        <p className="menuitem-thumbnail-price">
+          {this.props.price}
+        </p>
+      </div>
+      <div className="menuitem-thumbnail-buttons">
+        <img src={require('../../assets/img/add_item.png')} className="menuitem-thumbnail-add-item"></img>
+        <img src={require('../../assets/img/remove_item.png')} className="menuitem-thumbnail-remove-item"></img>
+      </div>
+    </div>;
+  },
+
+  _onRoomJoin: function() {
+    this.props.onRoomJoin(this.props.id);
+  }
+
+
+});
