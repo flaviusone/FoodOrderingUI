2684156a5ca37f7234deb47048411eb276b0472f
flaviusone <flaviusone@gmail.com>
2016-01-14 01:37:55 +0200

    Port changes

--- devServer.js
+++ devServer.js
@@ -87,7 +87,7 @@ io.on('connection', function (socket){
  * Normalize a port into a number, string, or false.
  */
 
-function val){
+function normalizePort(val) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
